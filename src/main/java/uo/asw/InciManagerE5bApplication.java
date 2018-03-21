3ec1e17941969fda71f84b9cd038971be8a00aba
ManuelMori <32633315+ManuelMori@users.noreply.github.com>
2018-03-21 19:40:50 +0100

    Revert "Añadido parser de JSON a incidencia y application.properties modificado"
    This reverts commit a7ddaba9954ddf52fd956accb1ced9e8241b2f8a.

--- src/main/java/uo/asw/InciManagerE5bApplication.java
+++ src/main/java/uo/asw/InciManagerE5bApplication.java
@@ -1,17 +1,12 @@
 package uo.asw;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class InciManagerE5bApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(InciManagerE5bApplication.class, args);
-		SpringApplication app = new SpringApplication(InciManagerE5bApplication.class);
-	    //app.setBannerMode(Banner.Mode.OFF);
-	    app.run(args);
+		SpringApplication.run(InciManagerE5bApplication.class, args);
 	}
 }
