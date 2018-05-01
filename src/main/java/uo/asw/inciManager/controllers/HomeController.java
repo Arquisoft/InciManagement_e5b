00978ef0d08bdf3299c1383a9746fedd1ae3ed8f
ManuelMori <32633315+ManuelMori@users.noreply.github.com>
2018-05-01 16:15:57 +0200

    Limpieza de codigo y reorganizacion en paquetes

---
+++ src/main/java/uo/asw/inciManager/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package uo.asw.inciManager.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+}
---
+++ src/main/java/uo/asw/inciManager/services/IncidenceService.java
@@ -0,0 +1,116 @@
+package uo.asw.inciManager.services;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import uo.asw.apacheKafka.producer.KafkaProducer;
+import uo.asw.dbManagement.model.Incidence;
+import uo.asw.dbManagement.model.Property;
+import uo.asw.dbManagement.repositories.IncidencesRepository;
+import uo.asw.reporter.InciReporter;
+
+@Service
+public class IncidenceService {
+
+	@Autowired
+	private IncidencesRepository incidenceRepository;
+
+	@Autowired
+	private KafkaProducer kafkaProducer;
+
+	private static final Logger logger = Logger.getLogger(KafkaProducer.class);
+
+	public void sendIncidence(Incidence incidence) {
+		kafkaProducer.send("incidences", generarJSON(incidence));
+	}
+
+	private String generarJSON(Incidence incidence) {
+		JSONObject json = new JSONObject();
+		json.put("identifier", incidence.getIdentifier());
+		json.put("login", incidence.getLogin());
+		json.put("password", incidence.getPassword());
+		json.put("kind", incidence.getKind());
+		json.put("name", incidence.getName());
+		json.put("description", incidence.getDescription());
+		json.put("location", incidence.getLocation());
+		json.put("tags", incidence.getTags());
+		json.put("properties", incidence.getProperties());
+		json.put("status", incidence.getStatus());
+		json.put("operatorIdentifier", incidence.getOperatorIdentifier());
+		json.put("expiration", incidence.getExpiration());
+
+		return json.toString();
+	}
+
+	public boolean manageIncidence(String name, String password, String kind, Incidence incidence) {
+		if (loginCorrecto(name, password, kind)) {
+			persistIncidence(incidence);
+			sendIncidence(incidence);
+			return true;
+		} else {
+			reportIncidence(incidence);
+			return false;
+		}
+	}
+
+	private void persistIncidence(Incidence incidence) {
+		incidenceRepository.save(incidence);
+	}
+
+	private boolean loginCorrecto(String name, String password, String kind) {
+		logger.info("Sending POST request to url http://localhost:8080/user ");
+		String url = "http://localhost:8080/user"; // Supuesta url desde donde
+													// se envían las peticiones
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(MediaType.APPLICATION_JSON);
+		JSONObject peticion = new JSONObject();
+		peticion.put("login", name);
+		peticion.put("password", password);
+		peticion.put("kind", kind);
+		HttpEntity<String> entity = new HttpEntity<String>(peticion.toString(), header);
+		ResponseEntity<String> response = new RestTemplate().exchange(url, HttpMethod.POST, entity, String.class);
+		HttpStatus responseCode = response.getStatusCode();
+		return responseCode.equals(HttpStatus.OK);
+	}
+
+	private void reportIncidence(Incidence incidence) {
+		InciReporter.reportInci(incidence);
+	}
+
+	public void listIncidence(Incidence incidence, String etiquetas, String propiedades) {
+		incidence.setTags(procesarString(incidence, etiquetas));
+		incidence.setProperties(procesarPropiedades(procesarString(incidence, propiedades)));
+	}
+
+	private Set<String> procesarString(Incidence incidence, String x) {
+		Set<String> list = new HashSet<String>();
+		for (String string : x.split(";")) {
+			list.add(string);
+		}
+		return list;
+	}
+
+	private Set<Property> procesarPropiedades(Set<String> list) {
+		Set<Property> list2 = new HashSet<Property>();
+		for (String s : list) {
+			String[] temp = s.split(":");
+			if (temp.length == 2) {
+				Property pro = new Property(temp[0], temp[1]);
+				list2.add(pro);
+			}
+		}
+		return list2;
+	}
+}
